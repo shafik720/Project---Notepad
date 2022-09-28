757471c2dd52dd5301e3709a1035a0c9277adb18
shafik720 <shafirkasel5@gmail.com>
2022-09-28 14:22:47 +0600

    dfgdfg

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
 // getting id from html document
-let heading = document.getElementById('heading');
+const row = document.getElementById('row');
+const heading = document.getElementById('heading');
+const noteTitle = document.getElementById('recipient-name')
+// const noteTitles = noteTitle.value;
+const noteBody = document.getElementById('message-text');
+// const noteBodys = noteBody.value; 
+const saveBtn = document.getElementById('saveBtn');
 
-heading.addEventListener('click', function(){
-    
+saveBtn.addEventListener('click',function(){
+    let div = document.createElement('div');
+    div.classList.add('col-lg-4');
+    div.innerHTML = `
+    <div class=" border border-3 rounded p-3 h-100">
+    <div class="note ">
+      <h4>${noteTitle.value}</h4>
+      <hr>
+      <p>${noteBody.value}</p>
+    </div>
+  </div>
+    `;
+    row.appendChild(div);
 })
+
