21c3c78d003eb2f7af0b6035a964e81a745f3a7d
shafik720 <shafirkasel5@gmail.com>
2022-09-28 17:18:22 +0600

    dsfgsd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 // getting id from html document
 const row = document.getElementById('row');
 const heading = document.getElementById('heading');
-const noteTitle = document.getElementById('recipient-name')
-const noteBody = document.getElementById('message-text');
-const saveBtn = document.getElementById('saveBtn');
-const edit = document.getElementById('edit');
+const noteTitle = document.getElementById('noteTitle')
+const noteBody = document.getElementById('noteContent');
+const createBtn = document.getElementById('createBtn');
+const edits = document.getElementsByClassName('edit');
 let noteHeaders = document.getElementsByClassName('noteTitle');
 let noteBodies = document.getElementsByClassName('noteBody');
 
-saveBtn.addEventListener('click',function(){
+createBtn.addEventListener('click',function(){
     let div = document.createElement('div');
     div.classList.add('col-lg-4');
     if(noteTitle.value || noteBody.value){
@@ -29,18 +29,8 @@ saveBtn.addEventListener('click',function(){
 })
 
 // making edit button functionable
-edit.addEventListener('click', function(event){
-  let noteHeaderText;
-  let noteBodyText;
-  for(let noteHeader of noteHeaders){
-    noteHeaderText = noteHeader.innerText;    
-  }
-  for(let noteBody of noteBodies){
-    noteBodyText = noteBody.innerText;
-  }
-  noteTitle.value = noteHeaderText;
-  noteBody.value = noteBodyText;
-  console.log(noteHeaderText);  
-  // console.log(event.target.parentNode.parentNode.classList);
-})
-
+for(let edit of edits){
+  edit.addEventListener('click', function(){
+    console.log('click');
+  })
+}
